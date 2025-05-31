66141ccc6e37837eb7b3d0260bee9e21c32511d1
tomast1337 <vycasnicolas@gmail.com>
2025-05-31 15:03:53 -0300

    feat: add blockchain-access package as a workspace dependency and update main entry point

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { initWeb3Provider } from "@event_ticketing/blockchain-access";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+initWeb3Provider().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
